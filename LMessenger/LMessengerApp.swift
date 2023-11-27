c504058f82837cda9ff23d81cafb943c8f1f2422
dodor <choidy9298@naver.com>
2023-11-27 19:06:08 +0900

    Initial Commit

---
+++ LMessenger/LMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMessengerApp.swift
+//  LMessenger
+//
+//  Created by dodor on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct LMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
